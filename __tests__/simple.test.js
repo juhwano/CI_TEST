406a7eea9be00f876f19f7a218aa131f79c550cb
juhwano <calculus9006@gmail.com>
2022-07-13 13:51:48 +0900

    suc test

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -6,10 +6,10 @@ test("1 + 2 = 3", () => {
   expect(1 + 2).toBe(3);
 });
 
-test("2 + 4 = 7", () => {
-  expect(2 + 4).toBe(7);
+test("2 + 4 = 6", () => {
+  expect(2 + 4).toBe(6);
 });
 
-test("2 + 4 = 8", () => {
-  expect(2 + 4).toBe(8);
+test("2 + 5 = 7", () => {
+  expect(2 + 5).toBe(7);
 });
